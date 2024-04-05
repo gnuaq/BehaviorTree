1e26025a5c23fd66e71c7d91c63ca7c36f4df630
minquang <91359738+minquang@users.noreply.github.com>
2024-04-05 17:46:07 +0700

    feat: add repeat node

---
+++ Assets/UnityBehaviorTree/Core/Decorator/Repeat.cs
@@ -0,0 +1,45 @@
+﻿using GraphProcessor;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace UnityBehaviorTree.Core.Decorator
+{
+    [System.Serializable, NodeMenuItem("BehaviorTree/Decorator/Repeat")]
+    public class Repeat : DecoratorNode
+    {
+        [SerializeField]
+        private int _numCycles = 0;
+
+        private int _currentCycle = 0;
+        protected override void OnStart()
+        {
+            _currentCycle = 0;
+        }
+
+        protected override void OnStop()
+        {
+        }
+
+        protected override EStatus OnUpdate()
+        {
+            for (int i = _currentCycle; i < _numCycles; i++)
+            {
+                _currentCycle = i;
+                Child.OnProcess();
+                switch (Child.Status)
+                {
+                    case EStatus.Success:
+                        continue;
+                    case EStatus.Running:
+                        return _status = EStatus.Running;
+                    case EStatus.Failed:
+                        return _status = EStatus.Failed;
+                    default:
+                        return _status = EStatus.Failed;
+                }
+            }
+
+            return _status = EStatus.Success;
+        }
+    }
+}
